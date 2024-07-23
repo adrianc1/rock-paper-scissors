f0d7deb33f5d554b7961cce891f0e6bcfd00c3ab
adriansito <adriancrosby02@gmail.com>
2024-07-23 00:08:21 -0700

    re-did javascript for this project. All within the console

--- main.js
+++ main.js
@@ -1,101 +1,58 @@
-let computerChoice;
-let userChoice;
-let randomNumber;
-let i = 0;
-let userScore = 0;
-let cpuScore = 0;
-let gameNumber = 0;
-let gameCounter = document.querySelector('.game-counter')
-let playerScore = document.querySelector('.player-score');
-let computerScore = document.querySelector('.computer-score')
-let playerDisplayChoice = document.querySelector('.player-choice')
-let computerDisplayChoice = document.querySelector('.computer-choice')
-let btnContainer = document.querySelectorAll('.btn-container');
+let computerScore = 0;
+let humanScore = 0;
+let total = computerScore + humanScore;
 
-function game(randomNumber, userChoice) {
-  console.log('it wokrs')
-    
-    // Computer Choice. Get random number and assign to value.
-    computerChoiceFunction(randomNumber);
+function getComputerChoice() {
+	const randomNumber = Math.floor(Math.random() * 3);
+	let computerChoice;
 
-    // Checks user vs computer choice and declares a winner
-    checkChoices(userChoice, computerChoice);
+	switch (randomNumber) {
+		case 0:
+			computerChoice = 'ROCK';
+			break;
+		case 1:
+			computerChoice = 'SCISSORS';
+			break;
+		case 2:
+			computerChoice = 'PAPER';
+			break;
+	}
 
-    isWinner(userScore, cpuScore);
-    
-};
-
-// Computer Choice Function 
-function computerChoiceFunction(randomNumber) {
-    if(randomNumber === 0) {
-        return computerChoice = "rock"
-    } else if (randomNumber === 1) {
-        return computerChoice = "paper"
-    } else if (randomNumber === 2) {
-        return computerChoice = "scissors"
-    } else {
-        return computerChoice = "FUCK"
-    }
-};
-
-// Checks the game choice
-function checkChoices(userChoice, computerChoice) {
-    gameNumber += 1
-    const choices = ["rock", "paper", "scissors"];
-    
-    // Validate user and computer choices
-    if (!choices.includes(userChoice) || !choices.includes(computerChoice)) {
-      alert("Invalid choice. Please select rock, paper, or scissors.") ;
-    };
-
-    playerDisplayChoice.innerText = userChoice.toUpperCase()
-    computerDisplayChoice.innerText = computerChoice.toUpperCase()
-    
-    // Determine the winner
-    if (userChoice === computerChoice) {
-      gameCounter.innerText = gameNumber
-      playerScore.innerText = userScore
-      computerScore.innerText = cpuScore
-      
-    } else if (
-      (userChoice === "rock" && computerChoice === "scissors") ||
-      (userChoice === "paper" && computerChoice === "rock") ||
-      (userChoice === "scissors" && computerChoice === "paper")
-    ) {
-        userScore += 1;
-        gameCounter.innerText = gameNumber
-        playerScore.innerText = userScore
-        computerScore.innerText = cpuScore
-    } else {
-        cpuScore += 1
-        gameCounter.innerText = gameNumber
-        playerScore.innerText = userScore
-        computerScore.innerText = cpuScore
-    }
-};
-
-function isWinner(userScore, cpuScore) {
-  if (userScore === 5) {
-    alert(`Game over! YOU win!`)
-    setTimeout(() => {location.reload()
-    }, 5000)
-    ;
-  } else if (cpuScore === 5) {
-    alert(`Game over! You LOSE!!!`)
-    setTimeout(() => {location.reload()
-    }, 5000)
-    ;
-    
-  }
+	return computerChoice;
 }
 
-  function userBtnChoice(button) {
-    button.addEventListener('click', () =>{
-      userChoice = button.innerText.toLowerCase();
-      randomNumber = Math.floor(Math.random() * 3);
-      game(randomNumber, userChoice);
-    })
-  }
+function getHumanChoice() {
+	const humanChoice = prompt(
+		'select a goddam rock, paper or scissors'
+	).toUpperCase();
 
-  btnContainer.forEach(userBtnChoice)
+	return humanChoice;
+}
 
+function playRound(human, computer) {
+	console.log(human, computer);
+	if (
+		(human === 'ROCK' && computer === 'SCISSORS') ||
+		(human === 'PAPER' && computer === 'ROCK') ||
+		(human === 'SCISSORS' && computer === 'PAPER')
+	) {
+		humanScore += 1;
+		console.log('HUMANITY WINS!');
+	} else if (human === computer) {
+		console.log('its a tie!');
+	} else {
+		computerScore += 1;
+		console.log('sorry, AI just took over!');
+	}
+	console.log(
+		`current score: human: ${humanScore}  computer : ${computerScore}`
+	);
+}
+
+while (total < 5) {
+	const computerSelection = getComputerChoice();
+	const humanSelection = getHumanChoice();
+
+	playRound(humanSelection, computerSelection);
+	total = computerScore + humanScore;
+}
